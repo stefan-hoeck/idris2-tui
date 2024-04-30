ad8eecfe8c9e06d6249109eb72d5fc64252a1a37
emdash <dotsony@gmail.com>
2024-04-29 23:29:10 -0700

    Add web server to input-shim.py
    Will handle image uploads from smartphones.

--- input-shim.py
+++ input-shim.py
@@ -6,16 +6,37 @@ printing a single stream of JSON-encoded packets to stdout.
 It takes care of placing the terminal in raw mode and restoring its
 state upon exit.
 
+It even spawns a local web server in order to handle image upload via
+smartphone.
+
 XXX: supress the broken pipe error if user quits from idris
+
 """
 
 import os
-import tty
-import sys
+import http.server
 import json
 import queue
-import threading
+import sys
 import termios
+import threading
+import tty
+
+# handles uploading images via a web interface
+#
+# point your phone at your hostname:8000
+def server(port, q):
+    class ImageHandler(http.server.SimpleHTTPRequestHandler):
+        def __init__(self, *args, **kwargs):
+            super().__init__(directory="web", *args, **kwargs)
+
+        def do_PUT(self):
+            print("test")
+            data = self.rfile.read(int(self.headers.get('content-length')))
+            q.put({"tag": "Image", "contents": [self.path, list(data)]})
+            self.send_response(200, "Ok")
+    s = http.server.HTTPServer(('', 8000), ImageHandler)
+    s.serve_forever()
 
 def readScale(path, q):
     device = open(path, "rb")
@@ -31,6 +52,11 @@ def readStdin(q):
 try:
     save = tty.setcbreak(sys.stdin.fileno())
     q = queue.Queue()
+    serverThread = threading.Thread(
+        target=server,
+        args=(8000, q),
+        daemon=True
+    )
     scaleThread = threading.Thread(
         target=readScale,
         args=(sys.argv[1], q),
@@ -43,6 +69,7 @@ try:
     )
     scaleThread.start()
     stdinThread.start()
+    serverThread.start()
     while True:
         json.dump(q.get(), sys.stdout)
         print("")
