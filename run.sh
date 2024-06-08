330042d0883c657f66e6da3d7c3624c94758dd6b
emdash <dotsony@gmail.com>
2024-06-08 16:30:33 -0700

    Display QR for the uploader server

--- run.sh
+++ run.sh
@@ -2,15 +2,32 @@
 
 export AMPII_DB_PATH=database
 export AMPII_SCALE_PATH=/dev/hidraw3
-export LINES
-export COLUMNS
 
-if test -e "${AMPII_SCALE_PATH}"
-then
-    python input-shim.py \
+function localURL {
+    echo "http://$(hostname -I | cut -d ' ' -f 1):8000"
+}
+
+function hostQR {
+    qrencode -o "upload/qrcode.png" "$(localURL)"
+}
+
+function shim {
+    # generate a QR code for this host
+    hostQR 2>&1 > /dev/null
+
+    # tell the app to load the image
+    echo '{"tag": "Image", "contents": ["upload/qrcode.png"]}'
+
+    # now actually start the shim
+    if test -e "${AMPII_SCALE_PATH}"
+    then
+	python input-shim.py \
 	   "${AMPII_SCALE_PATH}" \
-	   2>shim_log | ./build/exec/ampii "$@" 2> debug_log
-else
-    echo "Scale path does not exist"
-    python input-shim.py 2>shim_log | ./build/exec/ampii "$@" 2> debug_log
-fi
+	   2>shim_log
+    else
+	echo "Scale path does not exist"
+	python input-shim.py 2>shim_log
+    fi
+}
+
+shim | ./build/exec/ampii "$@" 2> debug_log
