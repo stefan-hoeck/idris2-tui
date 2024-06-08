81c27aa5b9fc944377642ed01b48decae236366f
emdash <dotsony@gmail.com>
2024-06-08 13:55:56 -0700

    Don't give scale path argument if it doesn't exist

--- run.sh
+++ run.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
 
 export AMPII_DB_PATH=database
-export AMPII_SCALE_PATH=/dev/hidraw0
+export AMPII_SCALE_PATH=/dev/hidraw3
 export LINES
 export COLUMNS
 
-python input-shim.py "${AMPII_SCALE_PATH}" | ./build/exec/ampii "$@" 2> debug_log
+if test -e "${AMPII_SCALE_PATH}"
+then
+    python input-shim.py \
+	   "${AMPII_SCALE_PATH}" \
+	   2>shim_log | ./build/exec/ampii "$@" 2> debug_log
+else
+    echo "Scale path does not exist"
+    python input-shim.py 2>shim_log | ./build/exec/ampii "$@" 2> debug_log
+fi
